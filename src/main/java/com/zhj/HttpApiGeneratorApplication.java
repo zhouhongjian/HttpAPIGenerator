72dbe8f686cc7f360727377bd5044127ee34445c
zhouhongjian <190303610@qq.com>
2017-11-26 23:33:41 +0800

    init

---
+++ src/main/java/com/zhj/HttpApiGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.zhj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpApiGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpApiGeneratorApplication.class, args);
+	}
+}
